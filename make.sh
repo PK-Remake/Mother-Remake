17f9148a58fbc58eb27eaae26a416176ffe8b3e9
ShadowOne333 <shadow_one666@hotmail.com>
2021-09-24 16:13:55 -0600

    Import some more songs (Fanfares are pending)

--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-coilsnake-cli compile ./coilsnake-project/ ./Base.smc ./MotherRemake.smc
+coilsnake-cli compile ./coilsnake-project/ Base.smc MotherRemake.smc
+coilsnake-cli createpatch "EarthBound (Expanded).smc" MotherRemake.smc ./Patches/MotherRemake.ips
+coilsnake-cli createpatch "EarthBound (Expanded).smc" MotherRemake.smc ./Patches/MotherRemake.ebp ShadowOne333 "Beta patch for the Mother Remake project" "Mother Remake"
